4b1c0ca22b0d217384568189d53bdaad2a892439
Gabriela Camacho <gabrielacacosta31@gmail.com>
2025-03-14 07:48:28 -0500

    cambios en las validaciones

--- validacionProducto.py
+++ validacionProducto.py
@@ -1,43 +1,42 @@
-#Entrada de la información por el usuario de línea 3 hasta la 10
+#Entrada de la información
 
 nombre = input("Ingrese el nombre del producto -> ")
 
-#Utilizamos la función float para convertir la información str a número decimal
-valor_producto = float(input('Ingrese el valor del producto -> '))
-
-#Utilizamos la función int para convertir la información str a números enteros
-cantidad = int(input("Ingrese la cantidad del producto -> "))
-descuento = int(input("Ingrese el porcentaje del descuento -> "))
-
-
-#Procesamiento de la línea 14 hasta la 40
+#Procesamiento de la línea 11 hasta la 38
 
 #Validación de que sí sea un número float y mayor que 0 para que el usuario no pueda ingresar un valor negativo, 
 # por lo tanto cada vez que ingrese un valor negativo se le repetira que ingrese un valor positivo hasta que se cumpla.
 #Así será para las demás validaciones y estaremos valindado que los números si sean enteros y mayor a 0.
 
-while not (type(valor_producto).__name__ == "float" and valor_producto > 0):
-  print("-------------------------------------------------------------")
-  print("El valor ingresado no es flotante o positivo" )
-  valor_producto = float(input("Ingresa un valor positivo para el producto -> "))
-print("-------")
-print("Valor correcto!")
+while True: 
+  try:
+    valor_producto = float(input('Ingrese el valor del producto -> '))
+    if (type(valor_producto).__name__ == "float" and valor_producto > 0):
+      break
+  except ValueError:
+    print("-------")
+    print("El valor que ingresaste no es valido")
+   
+
+while True: 
+  try:
+    cantidad = int(input("Ingrese la cantidad del producto -> "))
+    if (type(cantidad).__name__ == "int" and cantidad > 0):
+      break
+  except ValueError:
+    print("-------")
+    print("El valor que ingresaste no es valido")
 
 
-while not (type(cantidad).__name__ == "int" and cantidad > 0):
-   print("--------------------------------------------------")
-   print("La cantidad ingresada no es un número entero o positivo")
-   cantidad = int(input("Ingresa una cantidad positiva para el producto -> "))
-print("-------")
-print("Valor correcto!")
-
-
-#Acá validamos si el descuento esta dentro del rango de 0 a 100 y positivo
-while not 0 < descuento or descuento> 100:
-  print("-------------------------------------------------------")
-  print("Ingresa un número positivo para el porcentaje")
-  descuento = int(input("Ingrese el porcentaje del descuento -> "))
-print("Puedes continuar")
+while True: 
+  try:
+    descuento = int(input("Ingrese el porcentaje del descuento -> "))
+    #Acá validamos si el descuento esta dentro del rango de 0 a 100 y positivo
+    if type(descuento).__name__ == "int" or type(descuento).__name__ == "int" > 100:
+      break
+  except ValueError:
+    print("-------")
+    print("El valor que ingresaste no es valido")
 
 
 #Hacemos las respectivas operaciones para saber el costo con y sin descuento y la cantidad total a pagar.
